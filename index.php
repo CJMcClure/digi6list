33f23a1406a17becdf6814b798d9958881de35b0
CJMcClure <CJMcClure@fullsail.edu>
2016-11-01 22:09:27 -0400

    Created main list page, including form, and linked to css and js assets

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>pnp5 | Proof of Concept</title>
+	<meta charset="utf-8">
+	<link type="text/css" rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+	<h1>Digital 6 List</h1>
+	<ul>
+		<li data-item="0">
+			<input type="text" placeholder="Task" />
+			<input type="submit" value="+" class="add" />
+		</li>
+		<li data-item="1">
+			<input type="text" placeholder="Task" />
+			<input type="submit" value="+" class="add" />
+		</li>
+		<li data-item="2">
+			<input type="text" placeholder="Task" />
+			<input type="submit" value="+" class="add" />
+		</li>
+		<li data-item="3">
+			<input type="text" placeholder="Task" />
+			<input type="submit" value="+" class="add" />
+		</li>
+		<li data-item="4">
+			<input type="text" placeholder="Task" />
+			<input type="submit" value="+" class="add" />
+		</li>
+		<li data-item="5">
+			<input type="text" placeholder="Task" />
+			<input type="submit" value="+" class="add" />
+		</li>
+	</ul>
+	<script src="assets/js/mvc.js"></script>
+	<script src="assets/js/main.js"></script>
+</body>
+</html>
